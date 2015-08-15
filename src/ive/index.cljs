6e8993c93dd23a31a882cf34cb78492715e45605
Patrick K. O'Brien <patrick.keith.obrien@gmail.com>
2015-08-15 14:06:19 -0500

    Not needed

--- src/ive/index.cljs
+++ src/ive/index.cljs
@@ -1,9 +1,5 @@
 (ns ive.index
   (:require
-    [devcards.core]
     [ive.canvas-bouncing-ball]
     [ive.geometry]
     [ive.template]))
-
-(defn ^:export main []
-  (devcards.core/start-devcard-ui!))
